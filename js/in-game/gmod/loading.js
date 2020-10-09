e8da9ee9da05e8cff302d9d4d41fb23cf8bee60e
Fakas <matthewcaz@gmail.com>
2020-10-09 17:33:35 +0100

    Move Awesomeium-incompatible code into an if statement

--- js/in-game/gmod/loading.js
+++ js/in-game/gmod/loading.js
@@ -224,54 +224,55 @@ window.onload = function() {
     }
 
     if(!old) {
+        async function get_patrons() {
+            let response = await fetch(PATRONS_URL);
+            return await response.json();
+        }
+
+        async function show_patrons() {
+            let patrons = await get_patrons()
+            let loop = 1;
+            let patrons_html = "";
+            for (let ii in patrons) {
+                let patron = patrons[ii];
+                var name = patron["name"];
+                if (name.length > 18) {  // Shorten name length to something acceptable and abbreviate
+                    name = name.slice(0,17) + "…";
+                }
+                let tier = patron["tier"];
+                let dosh = patron["lifetime_amount"].toFixed(2);
+                let special = "";
+                if (loop === 1) {
+                    special = "legendary";
+                } else if (loop === 2) {
+                    special = "epic";
+                } else if (loop === 3) {
+                    special = "cool";
+                }
+                loop++;
+
+                patrons_html += `` +
+                    `<div class="patron ${special}">\n` +
+                    `    <div class="patron-name">\n` +
+                    `        ${name}\n` +
+                    `    </div>\n` +
+                    `    <div class="patron-tier">\n` +
+                    `        ${tier}\n` +
+                    `    </div>\n` +
+                    `    <div class=patron-amount>\n` +
+                    `        £${dosh}\n` +
+                    `    </div>\n` +
+                    `</div>`
+                if (ii >= 16) {
+                    break;
+                }
+            }
+            let board = document.getElementById("patrons");
+            board.innerHTML += patrons_html;
+            board.style.display = "block";
+        }
+
         show_patrons().then();
     }
 };
 
-async function get_patrons() {
-    let response = await fetch(PATRONS_URL);
-    return await response.json();
-};
-
-async function show_patrons() {
-    let patrons = await get_patrons()
-    let loop = 1;
-    let patrons_html = "";
-    for (let ii in patrons) {
-        let patron = patrons[ii];
-        var name = patron["name"];
-        if (name.length > 18) {  // Shorten name length to something acceptable and abbreviate
-            name = name.slice(0,17) + "…";
-        }
-        let tier = patron["tier"];
-        let dosh = patron["lifetime_amount"].toFixed(2);
-        let special = "";
-        if (loop === 1) {
-            special = "legendary";
-        } else if (loop === 2) {
-            special = "epic";
-        } else if (loop === 3) {
-            special = "cool";
-        }
-        loop++;
-
-        patrons_html += `` +
-            `<div class="patron ${special}">\n` +
-            `    <div class="patron-name">\n` +
-            `        ${name}\n` +
-            `    </div>\n` +
-            `    <div class="patron-tier">\n` +
-            `        ${tier}\n` +
-            `    </div>\n` +
-            `    <div class=patron-amount>\n` +
-            `        £${dosh}\n` +
-            `    </div>\n` +
-            `</div>`
-        if (ii >= 16) {
-            break;
-        }
-    }
-    let board = document.getElementById("patrons");
-    board.innerHTML += patrons_html;
-    board.style.display = "block";
-};
